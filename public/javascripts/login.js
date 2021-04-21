65fb39f867082bbe3b2d38394e18bea3262e9d77
Anderson Sanchez <andersonsanchez53@gmail.com>
2021-04-20 19:01:17 -0700

    fix bug

--- public/javascripts/login.js
+++ public/javascripts/login.js
@@ -5,25 +5,26 @@ $(() => {
             alert("Already logged in!")
             window.location.replace("/")
             // ...
+        } else {
+            // User is not logged in
+            $('#login').submit((e) => {
+                e.preventDefault()
+                const username = e.target.uid.value
+                const password = e.target.pass.value
+
+                firebase.auth().signInWithEmailAndPassword(username, password)
+                    .then((userCredential) => {
+                        // Signed in
+                        var user = userCredential.user;
+                        console.log("signed in")
+                        window.location.replace('/')
+                    })
+                    .catch((error) => {
+                        var errorCode = error.code;
+                        var errorMessage = error.message;
+                        $('#errorField').html(errorMessage)
+                    });
+            })
         }
     });
-
-    $('#login').submit((e) => {
-        e.preventDefault()
-        const username = e.target.uid.value
-        const password = e.target.pass.value
-
-        firebase.auth().signInWithEmailAndPassword(username, password)
-            .then((userCredential) => {
-                // Signed in
-                var user = userCredential.user;
-                console.log("signed in")
-                window.location.replace('/')
-            })
-            .catch((error) => {
-                var errorCode = error.code;
-                var errorMessage = error.message;
-                $('#errorField').html(errorMessage)
-            });
-    })
 })
