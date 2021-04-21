9e070c245277927aea3e5c634e1b5f51386bce56
Anderson Sanchez <andersonsanchez53@gmail.com>
2021-04-20 19:40:41 -0700

    reformat

--- public/javascripts/navbar.js
+++ public/javascripts/navbar.js
@@ -2,29 +2,31 @@ $(() => {
     firebase.auth().onAuthStateChanged((user) => {
         if (user) {
             $('#bigNav').append(`<ul class="navbar-nav navbar-right">
-            <li class="nav-item">
+                <li class="nav-item">
                 <button id="logoutBtn" class="btn" type="submit">logout</button>
-            </li>
-        </ul>`)
+                </li>
+                </ul>`
+            )
 
-        $("#logoutBtn").click(()=> {
-            firebase.auth().signOut().then(() => {
-                // Sign-out successful.
-                window.location.replace("/")
-              }).catch((error) => {
-                // An error happened.
-              });
-        })
+            $("#logoutBtn").click(() => {
+                firebase.auth().signOut().then(() => {
+                    // Sign-out successful.
+                    window.location.replace("/login")
+                }).catch((error) => {
+                    // An error happened.
+                    console.log(error)
+                });
+            })
         } else {
             $('#bigNav').append(`
-            <ul class="navbar-nav navbar-right">
+                <ul class="navbar-nav navbar-right">
                 <li class="nav-item">
-                    <a class="nav-link" href="/login">Log in</a>
+                <a class="nav-link" href="/login">Log in</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="/signup">Register</a>
+                <a class="nav-link" href="/signup">Register</a>
                 </li>
-            </ul>`
+                </ul>`
             )
         }
     });
